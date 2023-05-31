f821e88088ad8daea944c7851ff10c0fe5f7f588
alon suissa <alonsuissa12@gmail.com>
2023-05-31 10:07:46 +0300

    part A done

--- Assignments/Assignment 5/Part_A.c
+++ Assignments/Assignment 5/Part_A.c
@@ -1,3 +1,20 @@
 //
 // Created by alon on 5/31/23.
 //
+#include <math.h>
+// from gpt, request: write me a function in C that gets Unsigned int and checks if it's a prime number.
+int isPrime(unsigned int num) {
+    if (num < 2) {
+        return 0;
+    }
+
+    unsigned int limit = (unsigned int) sqrt(num);
+
+    for (unsigned int i = 2; i <= limit; i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
